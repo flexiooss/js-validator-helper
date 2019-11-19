79f1eb76f42ed3d0a22bc5bda7654fbb68473b82
tomacht <thomas@flexio.fr>
2019-11-19 14:54:06 +0100

    ObjectValidator

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ export {NumberValidator} from './src/js/Validator/NumberValidator'
 export {IntegerValidator} from './src/js/Validator/IntegerValidator'
 export {StringValidator} from './src/js/Validator/StringValidator'
 export {BooleanValidator} from './src/js/Validator/BooleanValidator'
+export {ObjectValidator} from './src/js/Validator/ObjectValidator'
 
 export {StringListValidator} from './src/js/Validator/list/StringListValidator'
 export {BooleanListValidator} from './src/js/Validator/list/BooleanListValidator'
