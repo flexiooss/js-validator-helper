9a7f74ea704bf61cd1fc2087d5acb01316f49dd7
Basheitan <antoine@flexio.fr>
2019-12-18 11:17:42 +0100

    add type date

---
+++ src/js/Validator/DateValidator.js
@@ -0,0 +1,66 @@
+import {Validator} from './Validator'
+import {isNull, isBoolean} from '@flexio-oss/assert'
+import {TypeCheck} from '@flexio-oss/flex-types'
+
+/**
+ * @implements {Validator}
+ */
+export class DateValidator extends Validator {
+  /**
+   *
+   * @param {FlexDate} value
+   * @return {boolean}
+   */
+  validateType(value) {
+    return TypeCheck.isFlexDate(value)
+  }
+
+  /**
+   *
+   * @param {FlexDate} value
+   * @return {boolean}
+   */
+  validateNotNull(value) {
+    return TypeCheck.isFlexDate(value) && !isNull(value)
+  }
+
+  /**
+   *
+   * @param {FlexDate} value
+   * @return {boolean}
+   */
+  validateNotEmpty(value) {
+    return this.validateNotNull(value)
+  }
+
+  /**
+   *
+   * @param {boolean} value
+   * @param {string} rangeStart
+   * @param {string} rangeEnd
+   * @return {boolean}
+   */
+  validateInRange(value, rangeStart, rangeEnd) {
+    throw new Error('DateValidator: no range for `validateInRange`')
+  }
+
+  /**
+   *
+   * @param {boolean} value
+   * @param {StringArray} enumeratedValues
+   * @return {boolean}
+   */
+  validateInEnumerated(value, enumeratedValues) {
+    throw new Error('DateValidator: no enumeratedValues for `validateRegex`')
+  }
+
+  /**
+   *
+   * @param {string} value
+   * @param {RegExp} regex
+   * @return {boolean}
+   */
+  validateRegex(value, regex) {
+    throw new Error('DateValidator: no regex for `validateRegex`')
+  }
+}
