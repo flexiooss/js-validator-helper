1ff57f8a507232769e3c92fa3b82a93471ae0ace
Basheitan <antoine@flexio.fr>
2020-03-31 17:33:05 +0200

    add duration validator

---
+++ src/js/Validator/DurationValidator.js
@@ -0,0 +1,72 @@
+import {isInteger, isNull} from '@flexio-oss/assert'
+import {Validator} from './Validator'
+import {globalFlexioImport} from '@flexio-oss/global-import-registry'
+import {TypeCheck} from '@flexio-oss/flex-types'
+
+/**
+ * @implements {Validator}
+ */
+export class DurationValidator extends Validator {
+  /**
+   *
+   * @param {Number} value
+   * @return {boolean}
+   */
+  validateType(value) {
+    return isInteger(value)
+  }
+  /**
+   *
+   * @param {Number} value
+   * @return {boolean}
+   */
+  validateNotNull(value) {
+    return this.validateType(value) && !isNull(value)
+  }
+
+  /**
+   *
+   * @param {Number} value
+   * @return {boolean}
+   */
+  validateNotEmpty(value) {
+    return this.validateNotNull(value)
+  }
+
+  /**
+   *
+   * @param {Number} value
+   * @param {string} rangeStart
+   * @param {string} rangeEnd
+   * @return {boolean}
+   */
+  validateInRange(value, rangeStart, rangeEnd) {
+    if(isNull(value)){
+      return true
+    }
+    return this.validateType(value) && value >= parseInt(rangeStart, 10) && value <= parseInt(rangeEnd,10)
+  }
+
+  /**
+   *
+   * @param {Number} value
+   * @param {StringArray} enumeratedValues
+   * @return {boolean}
+   */
+  validateInEnumerated(value, enumeratedValues) {
+    if(isNull(value)){
+      return true
+    }
+    return TypeCheck.isStringArray(enumeratedValues) && this.validateType(value) && enumeratedValues.mapTo(new globalFlexioImport.io.flexio.flex_types.arrays.IntegerArray(), v => parseInt(v,10)).includes(value)
+  }
+
+  /**
+   *
+   * @param {string} value
+   * @param {RegExp} regex
+   * @return {boolean}
+   */
+  validateRegex(value, regex) {
+    throw new Error('IntegerValidator: no regex for `validateRegex`')
+  }
+}
